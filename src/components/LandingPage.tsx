6189675d2cf15318129e88fe72067d82921e181f
Miguel Angel Casanova <tolkemada@gmail.com>
2024-05-10 00:34:28 +0200

    clean up

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,23 +2,23 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
-import {BookOpenCheck} from "lucide-react";
-import {SiFacebook, SiTwitter, SiYoutube} from "@icons-pack/react-simple-icons";
+import { BookOpenCheck } from "lucide-react";
+import { SiFacebook, SiTwitter, SiYoutube } from "@icons-pack/react-simple-icons";
 import TypewriterComponent from "typewriter-effect";
 
 const LandingPage = () => {
     return (
         <>
-            {/* Hero */}
+            {/* Hero */ }
             <section className="bg-[#062427]">
                 <div className="section-container flex flex-col text-white md:flex-row items-center">
-                    {/* Left */}
+                    {/* Left */ }
                     <div className="flex flex-col mb-32 space-y-12 text-center md:w-1/2 md:text-left">
                         <h1 className="max-w-md text-4xl md:text-5xl md:leading-tight">Chat with any PDF document</h1>
 
                         <div className="text-3xl font-light text-orange-400">
                             <TypewriterComponent
-                                options={{
+                                options={ {
                                     strings: [
                                         "Books",
                                         "Scientific papers",
@@ -28,12 +28,13 @@ const LandingPage = () => {
                                     ],
                                     autoStart: true,
                                     loop: true
-                                }}
+                                } }
                             />
                         </div>
 
                         <p className="max-w-md md:max-w-sm text-white/80 font-light leading-7">
-                            From legal agreements to financial reports, PDF Wisdom brings your documents to life. You can ask questions, get summaries, find information, and more.
+                            From legal agreements to financial reports, PDF Wisdom brings your documents to life. You
+                            can ask questions, get summaries, find information, and more.
                         </p>
 
                         <div>
@@ -43,34 +44,34 @@ const LandingPage = () => {
 
                             <div className="flex justify-start mt-6">
                                 <img className="h-6 w-6  my-auto object-cover rounded-full ring-2 ring-green-950"
-                                     src="/user_1.jpeg" alt=""/>
+                                       src="/user_1.jpeg" alt=""/>
                                 <img className="h-6 w-6  my-auto object-cover rounded-full ring-2 ring-green-950"
-                                     src="/user_2.jpeg" alt=""/>
+                                       src="/user_2.jpeg" alt=""/>
                                 <img className="h-6 w-6  my-auto object-cover rounded-full ring-2 ring-green-950"
-                                     src="/user_3.jpeg" alt=""/>
+                                       src="/user_3.jpeg" alt=""/>
                                 <img className="h-6 w-6  my-auto object-cover rounded-full ring-2 ring-green-950"
-                                     src="/user_4.jpeg" alt=""/>
+                                       src="/user_4.jpeg" alt=""/>
                                 <img className="h-6 w-6  my-auto object-cover rounded-full ring-2 ring-green-950"
-                                     src="/user_5.jpeg" alt=""/>
+                                       src="/user_5.jpeg" alt=""/>
                                 <p className="ml-2 my-auto text-sm text-slate-400">Loved by 100,000+ happy users</p>
                             </div>
                         </div>
                     </div>
 
-                    {/* Right */}
+                    {/* Right */ }
                     <div className="md:w-1/2">
                         <img src="/hero.svg" alt=""/>
                     </div>
                 </div>
             </section>
 
-            {/* Features */}
+            {/* Features */ }
             <section className="section-container">
                 <h1 className="text-center text-4xl font-semibold mb-5 sm:mb-10">
                     How it works
                 </h1>
                 <div className="text-black grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
-                    {/* Feature 1 */}
+                    {/* Feature 1 */ }
                     <div className="rounded-b-xl px-5 pb-5 pt-3 shadow-lg">
                         <div className="flex-col">
                             <div className="flex items-center justify-center">
@@ -83,7 +84,7 @@ const LandingPage = () => {
                         </div>
                     </div>
 
-                    {/* Feature 2 */}
+                    {/* Feature 2 */ }
                     <div className="rounded-b-xl px-5 pb-5 pt-3 shadow-lg">
                         <div className="flex-col">
                             <div className="flex items-center justify-center">
@@ -96,7 +97,7 @@ const LandingPage = () => {
                         </div>
                     </div>
 
-                    {/* Feature 3 */}
+                    {/* Feature 3 */ }
                     <div className="rounded-b-xl px-5 pb-5 pt-3 shadow-lg">
                         <div className="flex-col">
                             <div className="flex items-center justify-center">
@@ -111,7 +112,7 @@ const LandingPage = () => {
                 </div>
             </section>
 
-            {/* CTA */}
+            {/* CTA */ }
             <section className="section-container text-center">
                 <h1 className="font-semibold text-4xl">Get started</h1>
                 <p className="mt-6 mb-6 text-gray-500">
@@ -123,18 +124,18 @@ const LandingPage = () => {
                 </div>
             </section>
 
-            {/* Footer */}
+            {/* Footer */ }
             <footer className="bg-[#f8f5ee] py-10">
                 <div className="mx-auto max-w-7xl px-8 md:px-6">
-                    {/* Row 1 */}
+                    {/* Row 1 */ }
                     <div className="md:flex md:justify-between">
-                        {/* Logo */}
+                        {/* Logo */ }
                         <div className="flex items-start mb-6">
                             <BookOpenCheck className="w-8 h-8 mr-3"/>
                             <span className="text-xl font-medium">PDF Wisdom</span>
                         </div>
 
-                        {/* Links */}
+                        {/* Links */ }
                         <div className="grid grid-cols-2 gap-x-20">
                             <div>
                                 <h2 className="mb-4 text-sm font-medium">Universidad</h2>
@@ -157,7 +158,7 @@ const LandingPage = () => {
 
                     <hr className="my-6 border-gray-300 lg:my-8"/>
 
-                    {/* Row 2 */}
+                    {/* Row 2 */ }
                     <div className="text-sm tex-gray-500 sm:flex sm:items-center sm:justify-between">
                         <span>Copyright &copy; 2024, All Rights Reserved</span>
                         <div className="flex text-2xl space-x-6 sm:justify-center">
@@ -168,7 +169,7 @@ const LandingPage = () => {
                                 <SiFacebook className="w-5 h"/>
                             </a>
                             <a href="#">
-                                <SiYoutube className="w-5 h" />
+                                <SiYoutube className="w-5 h"/>
                             </a>
                         </div>
                     </div>
