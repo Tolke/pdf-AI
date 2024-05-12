07db3a6c068bb614c6e34aa8d54f7960501d894a
Miguel Angel Casanova <tolkemada@gmail.com>
2024-05-12 12:28:41 +0200

    Create prisma connection. Create database. Retrieve document from DB and show it. Format document data.

--- src/app/(dashboard)/documents/page.tsx
+++ src/app/(dashboard)/documents/page.tsx
@@ -1,76 +1,86 @@
 import UploadPDF from "@/components/UploadPDF";
-import { Button } from "@/components/ui/button";
-import { File, Pencil, Trash2, Upload } from "lucide-react";
+import { File, Pencil, Trash2 } from "lucide-react";
 import Link from "next/link";
+import React from "react";
+import prismaDb from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { formatDistanceToNow } from "date-fns";
+import { formatBytes } from "@/lib/utils";
 
-const Documents = () => {
-  const documents = [
-    { fileName: "User_Manual.pdf", fileSize: "1 MB", createdAt: "yesterday" },
-    { fileName: "Learn_Python.pdf", fileSize: "7 MB", createdAt: "3 days ago" },
-    {
-      fileName: "Google_Financial_Report.pdf",
-      fileSize: "25 MB",
-      createdAt: "4 weeks ago",
-    },
-  ];
 
-  return (
-    <section className="bg-[#faf9f6] min-h-screen">
-      <div className="section-container">
-        <div className="flex items-center justify-between mb-4">
-          <h1 className="text-2xl">Documents</h1>
-          <UploadPDF />
-        </div>
+const Documents = async () => {
+    const { userId } = auth();
+    const documents = await prismaDb.document.findMany({
+        orderBy: { createdAt: "desc" },
+        where: { userId: userId as string },
+    });
+    console.log('documents', documents);
 
-        <div className="bg-white rounded shadow w-full overflow-x-scroll">
-          <table className="min-w-full">
-            <tbody>
-              {documents.map((d, index) => (
-                <tr
-                  key={index}
-                  className={
-                    index === documents.length - 1
-                      ? ""
-                      : "border-b border-gray-200"
-                  }
-                >
-                  <td className="p-4 text-left flex items-center">
-                    <File
-                      className="w-4 h-4 mr-2"
-                      style={{ strokeWidth: "3" }}
-                    />
-                    <Link href="#">
-                      <span className="text-ellipsis overflow-hidden whitespace-normal max-w-[300px] text-sm font-medium">
-                        {d.fileName}
-                      </span>
-                    </Link>
-                  </td>
-                  <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
-                    {d.fileSize}
-                  </td>
-                  <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
-                    {d.createdAt}
-                  </td>
-                  <td className="p-4 text-right w-4">
-                    <Pencil
-                      className="w-4 h-4 cursor-pointer"
-                      style={{ strokeWidth: "3" }}
-                    />
-                  </td>
-                  <td className="p-4 text-right w-4">
-                    <Trash2
-                      className="w-4 h-4 cursor-pointer"
-                      style={{ strokeWidth: "3" }}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="bg-[#faf9f6] min-h-screen">
+            <div className="section-container">
+                <div className="flex items-center justify-between mb-4">
+                    <h1 className="text-2xl">Documents</h1>
+                    <UploadPDF/>
+                </div>
+
+                <div className="bg-white rounded shadow w-full overflow-x-scroll">
+                    <table className="min-w-full">
+                        <tbody>
+                        { documents.map((d, index) => (
+                            <tr
+                                key={ index }
+                                className={
+                                    index === documents.length - 1
+                                        ? ""
+                                        : "border-b border-gray-200"
+                                }
+                            >
+                                <td className="p-4 text-left flex items-center">
+                                    <File
+                                        className="w-4 h-4 mr-2"
+                                        style={ { strokeWidth: "3" } }
+                                    />
+                                    <Link href="#">
+                                      <span className="text-ellipsis overflow-hidden whitespace-normal max-w-[300px] text-sm font-medium">
+                                        { d.fileName }
+                                      </span>
+                                    </Link>
+                                </td>
+                                <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
+                                    { formatBytes(d.fileSize) }
+                                </td>
+                                <td className="p-4 text-right text-sm text-gray-500 whitespace-nowrap w-20">
+                                    { formatDistanceToNow(d.createdAt, { addSuffix: true }) }
+                                </td>
+                                <td className="p-4 text-right w-4">
+                                    <Pencil
+                                        className="w-4 h-4 cursor-pointer"
+                                        style={ { strokeWidth: "3" } }
+                                    />
+                                </td>
+                                <td className="p-4 text-right w-4">
+                                    <Trash2
+                                        className="w-4 h-4 cursor-pointer"
+                                        style={ { strokeWidth: "3" } }
+                                    />
+                                </td>
+                            </tr>
+                        )) }
+
+                        { documents.length === 0 && (
+                            <tr>
+                                <td className="p-4 text-center italic" colSpan={ 5 }>
+                                    No documents found
+                                </td>
+                            </tr>
+                        ) }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Documents;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,6 +50,17 @@ export function convertToPlainObj(obj: any) {
     return JSON.parse(JSON.stringify(obj));
 }
 
+export function formatBytes(bytes: number, decimals = 2): string {
+    if (bytes === 0) return "0 Bytes";
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
 function _getUrl(url: string | URL) {
     try {
         return new URL(url)
